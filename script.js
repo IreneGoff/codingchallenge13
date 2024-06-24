8e11bf94fbf3e515592aff02aeeb2a4cd9816a01
Irene Goff <ireneegoff@gmail.com>
2024-06-24 18:22:41 -0400

    Fetch and display product data from API.

---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const apiEndpoint = 'https://course-api.com/react-store-products';
+    const loadingElement = document.getElementById('loading');
