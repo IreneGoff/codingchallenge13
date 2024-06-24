eeaa3ed62d7eb20fd3103af09a4d61505109c69f
Irene Goff <ireneegoff@gmail.com>
2024-06-24 18:27:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//U35549532
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const apiEndpoint = 'https://course-api.com/react-store-products';
     const loadingElement = document.getElementById('loading');
