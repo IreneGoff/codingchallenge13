25cc1e67caa5bcb9763ee09578c7f65f1ab883ad
Irene Goff <ireneegoff@gmail.com>
2024-06-24 18:24:04 -0400

    Implement error handling for data fetching.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
     const apiEndpoint = 'https://course-api.com/react-store-products';
     const loadingElement = document.getElementById('loading');
+    const errorElement = document.getElementById('error');
